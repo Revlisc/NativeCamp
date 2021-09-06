38e1a6938562dc8fb65f62d71b5fe3af2c2b2960
dales <dalesimmons0210@gmail.com>
2021-09-05 21:32:04 -0700

    tried bug fixing

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,34 +1,26 @@
-import React, { Component } from 'react';
-import  { View, Platform, ScrollView, Text }  from 'react-native';
-import { Card } from 'react-native-elements';
+import React, { Component } from "react";
+import { Text, Card } from "react-native-elements";
+import { ScrollView } from "react-native-gesture-handler";
 
 class Contact extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+  static navigationOptions = {
+    title: "Contact Us",
+  };
 
-        }
-    }
-    static navigationOptions = {
-        title: 'Contact'
-    }
+  render() {
+    return (
+      <ScrollView>
+        <Card title="Contact Information" wrapperStyle={{ margin: 20 }}>
+          <Text>1 Nucamp Way</Text>
+          <Text>Seattle, WA 98001</Text>
+          <Text style={{ marginBottom: 10 }}>U.S.A</Text>
 
-    render() {
-        return (
-            <ScrollView >
-                <Card
-                    title='Contact Information'
-                    wrapperStyle={{margin: 20}}
-                >
-                    <Text>1 Nucamp Way</Text>
-                    <Text>Seattle, WA 98001</Text>
-                    <Text style={{marginBottom: 10}}>U.S.A</Text>
-                    <Text>Phone: 1-206-555-1234</Text>
-                    <Text>Email: campsites@nucamp.co</Text>
-                </Card>
-            </ScrollView>
-        )
-    }
+          <Text>Phone: 1-206-555-1234</Text>
+          <Text>Email: campsites@nucamp.co</Text>
+        </Card>
+      </ScrollView>
+    );
+  }
 }
 
 export default Contact;
