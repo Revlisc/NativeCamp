4f0343897c979a5e017b1022f5a850d672813457
dales <dalesimmons0210@gmail.com>
2021-09-06 23:53:52 -0700

    animatable

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -5,12 +5,23 @@ import { Text, Card, ListItem } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
 import Loading from "./LoadingComponent";
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = (state) => {
   return {
     partners: state.partners,
   };
 };
+
+function Mission() {
+  return (
+      <Card title='Our Mission'>
+          <Text style={{margin: 10}}>
+              We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
+          </Text>
+      </Card>
+  );
+}
 
 class About extends Component {
   static navigationOptions = {
@@ -32,15 +43,7 @@ class About extends Component {
       return (
         <ScrollView>
           <Card title="Our Mission">
-            <Text style={{ margin: 10 }}>
-              We present a curated database of the best campsites in the vast
-              woods and backcountry of the World Wide Web Wilderness. We
-              increase access to adventure for the public while promoting safe
-              and respectful use of resources. The expert wilderness trekkers on
-              our staff personally verify each campsite to make sure that they
-              are up to our standards. We also present a platform for campers to
-              share reviews on campsites they have visited with each other.
-            </Text>
+            <Mission />
           </Card>
           <Card title="Community Partners">
             <Loading />
@@ -51,45 +54,30 @@ class About extends Component {
 
     if (this.props.partners.errMess) {
       return (
-        <ScrollView>
-          <Card title="Our Mission">
-            <Text style={{ margin: 10 }}>
-              We present a curated database of the best campsites in the vast
-              woods and backcountry of the World Wide Web Wilderness. We
-              increase access to adventure for the public while promoting safe
-              and respectful use of resources. The expert wilderness trekkers on
-              our staff personally verify each campsite to make sure that they
-              are up to our standards. We also present a platform for campers to
-              share reviews on campsites they have visited with each other.
-            </Text>
-          </Card>
-          <Card title="Community Partners">
-            <Text>{this.props.partners.errMess}</Text>
-          </Card>
-        </ScrollView>
+          <ScrollView>
+              <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                  <Mission />
+                  <Card
+                      title="Community Partners">
+                      <Text>{this.props.partners.errMess}</Text>
+                  </Card>
+              </Animatable.View>
+          </ScrollView>
       );
-    }
-
-    return (
+  }
+  return (
       <ScrollView>
-        <Card title="Our Mission">
-          <Text style={{ margin: 10 }}>
-            We present a curated database of the best campsites in the vast
-            woods and backcountry of the World Wide Web Wilderness. We increase
-            access to adventure for the public while promoting safe and
-            respectful use of resources. The expert wilderness trekkers on our
-            staff personally verify each campsite to make sure that they are up
-            to our standards. We also present a platform for campers to share
-            reviews on campsites they have visited with each other.
-          </Text>
-        </Card>
-        <Card title="Community Partners">
-          <FlatList
-            data={this.props.partners.partners}
-            renderItem={renderPartner}
-            keyExtractor={(partner) => partner.id.toString()}
-          />
-        </Card>
+          <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+              <Mission />
+              <Card
+                  title="Community Partners">
+                  <FlatList
+                      data={this.props.partners.partners}
+                      renderItem={renderPartner}
+                      keyExtractor={item=>item.id.toString()}
+                  />
+              </Card>
+          </Animatable.View>
       </ScrollView>
     );
   }
--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -6,7 +6,7 @@ import { ScrollView } from "react-native-gesture-handler";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
 import { postFavorite, postComment } from "../redux/ActionCreators";
-import { TouchableHighlightBase } from "react-native";
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = (state) => {
   return {
@@ -27,34 +27,36 @@ function RenderCampsite(props) {
 
   if (campsite) {
     return (
-      <Card
-        featuredTitle={campsite.name}
-        image={{ url: baseUrl + campsite.image }}
-      >
-        <Text style={{ margin: 10 }}>{campsite.description}</Text>
-        <View style={styles.formRow}>
-          <Icon
-            name={props.favorite ? "heart" : "heart-o"}
-            type="font-awesome"
-            color="#f50"
-            raised
-            reverse
-            onPress={() =>
-              props.favorite
-                ? console.log("Already set as a favorite")
-                : props.markFavorite()
-            }
-          />
-          <Icon
-            name="pencil"
-            type="font-awesome"
-            color="#5637dd"
-            raised
-            reverse
-            onPress={() => props.onShowModal()}
-          />
-        </View>
-      </Card>
+      <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>  
+          <Card
+          featuredTitle={campsite.name}
+          image={{ url: baseUrl + campsite.image }}
+          >
+          <Text style={{ margin: 10 }}>{campsite.description}</Text>
+          <View style={styles.formRow}>
+            <Icon
+              name={props.favorite ? "heart" : "heart-o"}
+              type="font-awesome"
+              color="#f50"
+              raised
+              reverse
+              onPress={() =>
+                props.favorite
+                  ? console.log("Already set as a favorite")
+                  : props.markFavorite()
+              }
+            />
+            <Icon
+              name="pencil"
+              type="font-awesome"
+              color="#5637dd"
+              raised
+              reverse
+              onPress={() => props.onShowModal()}
+            />
+          </View>
+        </Card>
+      </Animatable.View>
     );
   }
   return <View />;
@@ -78,13 +80,15 @@ function RenderComments({ comments }) {
   };
 
   return (
-    <Card title="Comments">
-      <FlatList
-        data={comments}
-        renderItem={renderCommentItem}
-        keyExtractor={(item) => item.id.toString()}
-      />
-    </Card>
+    <Animatable.View animation='fadeInUp' duration={2000} delay={1000}>  
+      <Card title="Comments">
+        <FlatList
+          data={comments}
+          renderItem={renderCommentItem}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </Card>
+    </Animatable.View>
   );
 }
 
